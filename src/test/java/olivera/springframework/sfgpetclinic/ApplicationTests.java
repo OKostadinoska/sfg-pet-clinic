8d069469f07a6555a4b65f30483914bd6b325540
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-09-17 12:26:30 +0200

    Initial Project Add

---
+++ src/test/java/olivera/springframework/sfgpetclinic/ApplicationTests.java
@@ -0,0 +1,13 @@
+package olivera.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

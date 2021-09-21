c9b30970ae8ef060176fb69561c7c899c0088c80
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-09-21 17:19:10 +0200

    refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package olivera.springframework.sfgpetclinic.services;
 
 import olivera.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService <Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }

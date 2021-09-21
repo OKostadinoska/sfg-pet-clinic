8444e20ca38b89cf50338fa567215c173158b962
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-09-21 12:52:12 +0200

    implement interfaces services. Closes #5.

---
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package olivera.springframework.sfgpetclinic.services;
+
+import olivera.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package olivera.springframework.sfgpetclinic.services;
+
+import olivera.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}

9f68e30941278d2651d464df3f448cf19d2e0b0e
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-10-01 11:22:48 +0200

    Create PetType Map Service Impl. Closes #45.

---
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package olivera.springframework.sfgpetclinic.services;
+
+import olivera.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}

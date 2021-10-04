acb579a015498394423420236109dbd1bd63879c
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-10-04 09:03:05 +0200

    Create Specialty Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package olivera.springframework.sfgpetclinic.services;
+
+import olivera.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends  CrudService<Specialty, Long> {
+}

3a570387ac2c201a775680192da593a87ad7dac3
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-10-06 12:45:50 +0200

    Create Visit Map Service . Closes #47.

---
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package olivera.springframework.sfgpetclinic.services;
+
+import olivera.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}

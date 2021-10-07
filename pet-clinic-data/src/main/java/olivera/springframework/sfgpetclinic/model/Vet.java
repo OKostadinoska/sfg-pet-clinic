3553900439c58969e215015e3f5624eb37487759
OKostadinoska <kostadinoskaolivera@outlook.com>
2021-10-07 10:38:53 +0200

    Add Project Lombok and Refactor . Closes #52.

--- pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/olivera/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package olivera.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,11 +21,4 @@ public class Vet extends Person {
         inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
